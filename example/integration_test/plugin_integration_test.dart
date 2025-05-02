3f2613d741e5537320cbd78a2977294b0b779f4a
molihuan <2369902117@qq.com>
2025-05-02 21:24:29 +0800

    去除integration_test,修复鸿蒙因integration_test编译错误

--- example/integration_test/plugin_integration_test.dart
+++ example/integration_test/plugin_integration_test.dart
@@ -8,18 +8,18 @@
 
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:integration_test/integration_test.dart';
+// import 'package:integration_test/integration_test.dart';
 
 import 'package:flutterplugstudy/flutterplugstudy.dart';
 
 void main() {
-  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  // IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets('getPlatformVersion test', (WidgetTester tester) async {
-    final Flutterplugstudy plugin = Flutterplugstudy();
-    final String? version = await plugin.getPlatformVersion();
-    // The version string depends on the host platform running the test, so
-    // just assert that some non-empty string is returned.
-    expect(version?.isNotEmpty, true);
-  });
+  // testWidgets('getPlatformVersion test', (WidgetTester tester) async {
+  //   final Flutterplugstudy plugin = Flutterplugstudy();
+  //   final String? version = await plugin.getPlatformVersion();
+  //   // The version string depends on the host platform running the test, so
+  //   // just assert that some non-empty string is returned.
+  //   expect(version?.isNotEmpty, true);
+  // });
 }
